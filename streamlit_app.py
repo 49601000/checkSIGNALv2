2b003fdeaa5862eb2e344d2caf0dad5939ea728d
49601000 <the3stigmataofpalmereldritch@gmail.com>
2025-12-08 06:24:12 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,6 @@
 import sys
 import os
 
-# --- プロジェクトルートを sys.path に追加 ---
-ROOT = os.path.dirname(os.path.abspath(__file__))
-sys.path.append(ROOT)
-sys.path.append(os.path.join(ROOT, "app"))
-sys.path.append(os.path.join(ROOT, "modules"))
-
 from app.main import main
 
 if __name__ == "__main__":
